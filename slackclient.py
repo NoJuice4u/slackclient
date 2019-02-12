7770aa5fe237b097eadb37542b8b431673cb5c46
Wayne Chen <jungwnr@hotmail.com>
2019-02-12 15:06:14 +0900

    remove useage of channels.list as it is deprecated, added ability to change channels

--- slackclient.py
+++ slackclient.py
@@ -1,5 +1,6 @@
 import requests
 import datetime
+import random
 
 class SlackError(Exception):
     pass
@@ -13,14 +14,14 @@ class SlackClient(object):
             for line in inFile:
                 propertyLine = line.split('=')
                 self.properties[propertyLine[0]] = propertyLine[1].rstrip()
-                
+
         self.verify = verify
         self.blocked_until = None
         self.channel_name_id_map = {}
-        requests.packages.urllib3.disable_warnings()
+    
+        self.channel = self.channel_name_to_id(self.properties['default.channel'][1:])
 
-    def _channel_is_name(self, channel):
-        return channel.startswith('#')
+        requests.packages.urllib3.disable_warnings()
 
     def _make_request(self, method, as_user, params):
         if(self.blocked_until is not None and datetime.datetime.utcnow() < self.blocked_until):
@@ -46,28 +47,49 @@ class SlackClient(object):
         except:
             print("JSON Parse Exception!  Send message to Slack may have failed!")
             print(response.status_code)
+            print(params)
             print(response)
         
         return response
         
-    def channels_list(self, exclude_archived=True, **params):
-        method = 'channels.list'
-        params.update({'exclude_archived': exclude_archived and 1 or 0})
+    def channels_list(self, cursor=None, exclude_archived=True, **params):
+        method = 'conversations.list'
+        params.update({
+                'exclude_archived': exclude_archived and 1 or 0,
+                'limit': 1000,
+                'types': "public_channel"})
+        if(cursor is not None):
+            params.update({'cursor': cursor})
+        print(params)
+        return self._make_request(method, True, params)
+        
+    def channels_info(self, channel, **params):
+        method = 'channels.info`'
+        params.update({'channel': channel})
         return self._make_request(method, True, params)
         
     def channel_name_to_id(self, channel_name, force_lookup=False):
         if(force_lookup or not self.channel_name_id_map):
-            channels = self.channels_list().json()['channels']
-            self.channel_name_id_map = {channel['name']: channel['id'] for channel in channels}
+            cursor = None
+            while(cursor != ''):
+                channelsResponse = self.channels_list(cursor = cursor)
+                channels = channelsResponse.json()['channels']
+                for channel in channels:
+                    self.channel_name_id_map[channel['name']] = channel['id']                
+                cursor = channelsResponse.json()['response_metadata']['next_cursor']
+                print(cursor)
+                
         channel = channel_name.startswith('#') and channel_name[1:] or channel_name
+        print("SET Channel: " + channel)
         return self.channel_name_id_map.get(channel)
+    
+    def get_channel_map(self):
+        return self.channel_name_id_map
         
-    def chat_read(self, channel, ts, **params):
+    def chat_read(self, ts, **params):
         method = 'channels.history'
-        if self._channel_is_name(channel):
-            channel = self.channel_name_to_id(channel)
         params.update({
-            'channel': channel
+            'channel': self.channel
         })
         if(ts is not None):
             params.update({
@@ -75,26 +97,26 @@ class SlackClient(object):
             })
         return self._make_request(method, True, params)
     
-    def chat_post_message(self, channel, text, **params):
-        image = self.properties['bot.avatar']
-        return self.chat_post_message_customImage(channel, text, image, **params)
+    def chat_post_message(self, text, **params):
+        images = self.properties['bot.avatar'].split(",")
+        image = random.choice(images)
+        
+        return self.chat_post_message_customImage(text, image, **params)
     
-    def chat_post_message_customImage(self, channel, text, image, **params):
+    def chat_post_message_customImage(self, text, image, **params):
         method = 'chat.postMessage'
         params.update({
-            'channel': channel,
+            'channel': self.channel,
             'text': ">>>" + text,
             'icon_url': image,
             'as_user': False
         })
         return self._make_request(method, False, params)
     
-    def chat_delete_message(self, channel, ts, **params):
+    def chat_delete_message(self, ts, **params):
         method = 'chat.delete'
-        if self._channel_is_name(channel):
-            channel = self.channel_name_to_id(channel)
         params.update({
-            'channel': channel,
+            'channel': self.channel,
             'ts': ts
         })
         return self._make_request(method, False, params)
@@ -105,6 +127,12 @@ class SlackClient(object):
             'bot': self.properties['bot.id']
         })
         return self._make_request(method, False, params)
+    
+    def changeChannel(self, channel):
+        if channel.startswith('<'):
+            print(channel.split("|")[0][2:])
+            self.channel = channel.split("|")[0][2:]
+        print("Channel Change: " + str(self.channel))
 
 #client = SlackClient()
 #channel = '#bot_sandbox'
